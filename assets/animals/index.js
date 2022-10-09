762c3edf0b02a0ec65315a9e36cb4fd779789ebc
Valeria <valeria.yanycheva@gmail.com>
2022-10-09 23:53:53 +0600

    refactor: change path of the animals, collect information about all animals in one file

---
+++ assets/animals/index.js
@@ -0,0 +1,11 @@
+const animals = {
+    animals1 : '../../assets/animals/animals-1.jpg',
+    animals2 : '../../assets/animals/animals-2.jpg',
+    animals3 : '../../assets/animals/animals-3.jpg',
+    animals4 : '../../assets/animals/animals-4.jpg',
+    animals5 : '../../assets/animals/animals-5.jpg',
+    animals6 : '../../assets/animals/animals-6.jpg',
+    animals7 : '../../assets/animals/animals-7.jpg',
+}
+
+export default animals;
---
+++ data/animals.js
@@ -0,0 +1,48 @@
+import animals from '../assets/animals/index.js'
+
+const animalsData = [
+    {
+        images: animals.animals1,
+        name: 'giant Pandas',
+        location: 'Native to Southwest China',
+        icon: 'banana'
+    },
+    {
+        images: animals.animals2,
+        name: 'Eagles',
+        location: 'Native to South America',
+        icon: 'meat'
+    },
+    {
+        images: animals.animals3,
+        name: 'Gorillas',
+        location: 'Native to Congo',
+        icon: 'banana'
+    },
+    {
+        images: animals.animals4,
+        name: 'Two-toed Sloth',
+        location: 'Mesoamerica, South America',
+        icon: 'banana'
+    },
+    {
+        images: animals.animals5,
+        name: 'cheetahs',
+        location: 'Native to Africa',
+        icon: 'meat'
+    },
+    {
+        images: animals.animals6,
+        name: 'Penguins',
+        location: 'Native to Antarctica',
+        icon: 'meat'
+    },
+    {
+        images: animals.animals7,
+        name: 'Alligators',
+        location: 'Native to United States',
+        icon: 'meat'
+    },
+]
+
+export default animalsData;
