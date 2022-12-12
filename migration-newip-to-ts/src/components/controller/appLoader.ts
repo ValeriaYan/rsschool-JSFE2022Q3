d7511206f2f03c073f8754109b2d00e8b816f489
Valeria <valeria.yanycheva@gmail.com>
2022-12-13 00:40:52 +0600

    fix: change link to proxy for right deploy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '07b9d49d4ea94d17a47ddc3c1474eb68', // получите свой ключ https://newsapi.org/
         });
     }
